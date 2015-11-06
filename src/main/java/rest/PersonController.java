cab07ad996b1f4c39b3d3fe5a4959d074d93b6f0
hangjiliang <hangjiliang@gmail.com>
2015-11-06 17:38:08 -0600

    controller

--- src/main/java/rest/PersonController.java
+++ src/main/java/rest/PersonController.java
@@ -1,5 +1,23 @@
 package rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import databaseService.DerbyDatabaseService;
+
+@Controller
+@RequestMapping("/accounts")
 public class PersonController {
+	
+	private DerbyDatabaseService _service;
+
+	@Autowired
+	public PersonController(DerbyDatabaseService _service) {
+		super();
+		this._service = _service;
+	}
+	
+	
 
 }
