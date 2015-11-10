3cbd80603006300776cfa691cee52ebc981529a1
hangjiliang <hangjiliang@gmail.com>
2015-11-10 14:15:45 -0600

    controller init

--- src/main/java/rest/PersonController.java
+++ src/main/java/rest/PersonController.java
@@ -2,22 +2,34 @@ package rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import databaseService.DerbyDatabaseService;
+import databaseService.DatabaseService;
+import entity.Person;
 
 @Controller
 @RequestMapping("/accounts")
 public class PersonController {
 	
-	private DerbyDatabaseService _service;
+	private DatabaseService _service;
 
 	@Autowired
-	public PersonController(DerbyDatabaseService _service) {
+	public PersonController(DatabaseService _service) {
 		super();
 		this._service = _service;
 	}
 	
-	
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public @ResponseBody Person getPerson(@PathVariable String name){
+		Person p = _service.findPerson(name);
+		if(p != null){
+			return p;
+		}else{
+			return null;
+		}
+	}
 
 }
