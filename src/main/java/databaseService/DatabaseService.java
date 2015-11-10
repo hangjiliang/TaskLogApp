ef7f9b53392acb132e20b68e0bcc866be129c7a2
hangjiliang <hangjiliang@gmail.com>
2015-11-10 14:07:57 -0600

    extract interface

---
+++ src/main/java/databaseService/DatabaseService.java
@@ -0,0 +1,30 @@
+package databaseService;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+
+import entity.Person;
+import entity.TaskLog;
+
+public interface DatabaseService {
+
+	public abstract void savePerson(Person user);
+
+	public abstract List<Person> getAllPerson();
+
+	public abstract Person findPerson(String name);
+
+	public abstract void saveTaskLog(TaskLog task);
+
+	public abstract List<TaskLog> getAllTaskLogs();
+
+	public abstract TaskLog findTaskLog(String task);
+
+	public abstract void updateTaskLog(String personName, TaskLog task);
+
+	public abstract void deletePerson(String personName);
+
+	public abstract void deleteTaskLog(String task);
+
+}
